744039d5615d1ca7ae9f8c0f56701146f587518c
joohyoungkim19940805 <oozu1994@gmail.com>
2024-11-08 00:47:49 +0900

    ㄴ

--- src/app/@modal/(.)modal/[modal_name]/layout.tsx
+++ src/app/@modal/(.)modal/[modal_name]/layout.tsx
@@ -1,15 +1,16 @@
 import GlobalModalWrapper from '@/components/modal/GlobalModalWrapper';
 import { ReactNode } from 'react';
 import { ModalType } from '@/app/@modal/(.)modal/[modal_name]/@types/ModalType';
+
 interface ModelLayoutProps {
     children: ReactNode;
     params: {
         modal_name: ModalType;
     };
-    types?: string[]; // 선택적 속성으로 지정
+    types: string[]; // 필수 속성으로 지정
 }
 
-const ModelLayout = ({ children, params, types = [] }: ModelLayoutProps) => {
+const ModelLayout = ({ children, params, types }: ModelLayoutProps) => {
     const { modal_name: modalName } = params;
     return (
         <GlobalModalWrapper isOpen={true} type={modalName}>
